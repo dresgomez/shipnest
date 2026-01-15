33e8659100d4e0c754baf33f0d419a1cf0800803
unknown <inagas1526@gmail.com>
2026-01-15 01:59:04 -0300

    Debug PayPal env vars

--- api/checkout/create-paypal-order.js
+++ api/checkout/create-paypal-order.js
@@ -1,3 +1,8 @@
+console.log("ENV CHECK", {
+  hasId: !!process.env.PAYPAL_CLIENT_ID,
+  hasSecret: !!process.env.PAYPAL_CLIENT_SECRET
+});
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
