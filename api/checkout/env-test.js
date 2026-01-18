4422db0183776c154595324a3126742f2b71984c
unknown <inagas1526@gmail.com>
2026-01-18 02:21:15 -0300

    Remove env test endpoint (cleanup)

--- api/checkout/env-test.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || null,
-    PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET ? "OK" : null
-  });
-}
