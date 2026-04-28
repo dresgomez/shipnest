e66e059f32a0ca0aa8f04ab4a094b2eeb50928ee
unknown <inagas1526@gmail.com>
2026-04-28 00:54:20 -0300

    trust me babe

--- public/js/script.js
+++ public/js/script.js
@@ -47,46 +47,24 @@ data.products.forEach(product => {
 
 loadProducts();
 
-
-// =======================
-// UI productos
-// =======================
-
-function renderProducts() {
-  const container = document.getElementById("products-container");
-  if (!container) return;
-
-  container.innerHTML = "";
-
-  products.forEach(p => {
-    container.innerHTML += `
-      <div class="product-card">
-        <img src="${p.image}" alt="${p.name}">
-        <h3>${p.name}</h3>
-        <p class="price">$${(p.price / 100).toFixed(2)}</p>
-    <button 
-  class="add-cart-btn"
-  onclick='addToCart(${JSON.stringify(p)}, this)'
->
-  Add to Cart
-</button>
-      </div>
-    `;
-  });
-}
-
 // =======================
 // Inicialización
 // =======================
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderProducts();
+    loadProducts();
 
   if (typeof renderCart === "function") {
     renderCart();
   }
 
+  if (typeof updateCartCount === "function") {
+    updateCartCount();
+  }
+});
+
+window.addEventListener("pageshow", () => {
   if (typeof updateCartCount === "function") {
     updateCartCount();
   }
