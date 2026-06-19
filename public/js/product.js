de3c5c8ce0679b3676f5b3d6e7457010b24ab899
unknown <inagas1526@gmail.com>
2026-06-19 00:10:44 -0300

    pc ui upgrade 4

--- public/js/product.js
+++ public/js/product.js
@@ -23,6 +23,8 @@ async function loadProduct() {
 
   document.getElementById("product-image").src = product.image;
   document.getElementById("product-name").textContent = product.name;
+  document.getElementById("breadcrumb-product").textContent =
+  product.name;
   document.getElementById("product-price").textContent =
     "$" + (product.price / 100).toFixed(2);
 
