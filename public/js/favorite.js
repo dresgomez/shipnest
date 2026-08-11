0dda868ce43b796c9c14b4ff023215023d97eb50
unknown <inagas1526@gmail.com>
2026-08-11 00:06:04 -0300

    advanced state: client; favorites.js.5

--- public/js/favorite.js
+++ public/js/favorite.js
@@ -46,63 +46,71 @@ favoritesEmpty.style.display = "none";
 
 favoritesList.style.display = "flex";
 
-    favorites.forEach((favorite) => {
+ favorites.forEach((favorite) => {
 
-        const card = document.createElement("div");
+    const card = document.createElement("div");
 
-        card.classList.add("favorite-card");
+    card.classList.add("favorite-card");
 
-        card.innerHTML = `
+    card.innerHTML = `
 
-            <img 
-                src="https://placehold.co/120x120" 
-                alt="${favorite.name}"
-            >
+        <img 
+            src="https://placehold.co/120x120" 
+            alt="${favorite.name}"
+        >
 
-            <div class="favorite-info">
+        <div class="favorite-info">
 
-                <h3>${favorite.name}</h3>
+            <h3>${favorite.name}</h3>
 
-                <p>${favorite.price}</p>
+            <p>${favorite.price}</p>
 
-                <span>${favorite.seller}</span>
+            <span>${favorite.seller}</span>
 
-            </div>
+        </div>
 
-            <div class="favorite-actions">
+        <div class="favorite-actions">
 
-                <button class="details-btn">
-                    Ver producto
-                </button>
+            <button class="details-btn">
+                Ver producto
+            </button>
 
-                <button class="remove-favorite">
-                    <i class="fa-solid fa-trash"></i>
-                </button>
+            <button class="remove-favorite">
+                <i class="fa-solid fa-trash"></i>
+            </button>
 
-            </div>
+        </div>
 
-        `;
+    `;
 
-        favoritesList.appendChild(card);
+    favoritesList.appendChild(card);
 
-               const removeButton = card.querySelector(".remove-favorite");
 
-        removeButton.addEventListener("click", () => {
+    const removeButton = card.querySelector(".remove-favorite");
 
-    const index = favorites.indexOf(favorite);
+    const detailsButton = card.querySelector(".details-btn");
 
-    favorites.splice(index, 1);
 
-    renderFavorites();
+    removeButton.addEventListener("click", () => {
+
+        const index = favorites.indexOf(favorite);
+
+        favorites.splice(index, 1);
+
+        renderFavorites();
+
+    });
+
+
+    detailsButton.addEventListener("click", () => {
+
+        console.log("Ver producto:", favorite.name);
+
+    });
 
 
 });
 
-    });
-
 }
-
-
-    renderFavorites();
 
 });
